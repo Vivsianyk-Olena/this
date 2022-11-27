d7ea2ffbce72b3c8b6e7d5a0fa5f3476c5e5f036
Vivsianyk-Olena <vivsianykolena@gmail.com>
2022-11-28 01:04:37 +0200

    added date and weather icons

--- src/index.js
+++ src/index.js
@@ -1,19 +1,53 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "a68e9aa87440c646eff7900fe5a9a1d5";
-let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Zhytomyr&appid=a68e9aa87440c646eff7900fe5a9a1d5&units=metric";
+let city = "new york";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a68e9aa87440c646eff7900fe5a9a1d5&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
 
