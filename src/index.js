11bd55dc70de7e93ad477afcc391e0a9c6107ddc
Vivsianyk-Olena <vivsianykolena@gmail.com>
2022-11-30 02:15:43 +0200

    forecast (html in js)

--- src/index.js
+++ src/index.js
@@ -23,6 +23,35 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+ 
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+  forecastHTML = 
+  forecastHTML +
+  `
+    <div class="col">
+          
+          <div class="weather-forecast-date">
+          ${day}
+          </div>
+          <div class="weather-forecast-temperature">
+          <span class="weather-forecast-temperature-max">
+            18°
+          </span>
+          <span class="weather-forecast-temperature-min">
+            12°
+          </span>
+          </div>
+          <img src="image/rain.png" width="85px" />
+        </div>
+      `;
+  });
+      forecastHTML = forecastHTML + `</div>`;
+      forecastElement.innerHTML = forecastHTML;
+      }
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -90,3 +119,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Zhytomyr");
+displayForecast();
